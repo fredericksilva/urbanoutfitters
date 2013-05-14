fbe3d430d61bb1ba9945e6448448ac0a1a7adf20
jotismoore <jotismoore@me.com>
2013-05-14 14:59:25 +0100

    fixed signup bugs ready for go live

--- resources/js/Venda/Widget/modalSignup.js
+++ resources/js/Venda/Widget/modalSignup.js
@@ -11,7 +11,7 @@ Venda.Widget.modalSignup.popupCookieSubmit = new CookieJar({
 });
 Venda.Widget.modalSignup.Submit = 'submitted the popup';
 submittedPopup = Venda.Widget.modalSignup.popupCookieSubmit.get('submittedPopup');
-Venda.Widget.modalSignup = {
+Venda.Widget.modalSignup.action = {
     options: {
         eopts: "",
         w: "wrapper",
@@ -21,21 +21,23 @@ Venda.Widget.modalSignup = {
         de: "http://www.urbanoutfitters.de/page/home/&log=4"
     },
     init: function () {
-        Venda.Widget.modalSignup.displayPopup();
+    	Venda.Widget.modalSignup.popupCookieClose;
+		Venda.Widget.modalSignup.popupCookieSubmit;
+        Venda.Widget.modalSignup.action.displayPopup();
         jQuery('#modalSignup').submit(function (a) {
-            Venda.Widget.modalSignup.options.eopts = a;
-            Venda.Widget.modalSignup.modalSubmit(Venda.Widget.modalSignup.options.eopts, this);
+            Venda.Widget.modalSignup.action.options.eopts = a;
+            Venda.Widget.modalSignup.action.modalSubmit(Venda.Widget.modalSignup.action.options.eopts, this);
         });
     },
     addGender: function (gender) {
         jQuery('input[name=GENDER]').val(gender);
     },
     genderFunction: function (a) {
-        Venda.Widget.modalSignup.addGender(jQuery(a).data("gender"));
+        Venda.Widget.modalSignup.action.addGender(jQuery(a).data("gender"));
     },
     modalSubmit: function (a, b) {
         a.preventDefault();
-        if (Venda.Widget.modalSignup.checkForm(b)) {
+        if (Venda.Widget.modalSignup.action.checkForm(b)) {
             Venda.Widget.modalSignup.popupCookieSubmit.put('submittedPopup', Venda.Widget.modalSignup.Submit);
             submittedPopup = Venda.Widget.modalSignup.popupCookieSubmit.get('submittedPopup');
             var e = b.email.value,
@@ -58,8 +60,8 @@ Venda.Widget.modalSignup = {
 
                 function fadeBackOut() {
                     setTimeout(function () {
-                        jQuery('.' + Venda.Widget.modalSignup.options.w + ' .' + Venda.Widget.modalSignup.options.sP).css('display', 'none');
-                        jQuery('.' + Venda.Widget.modalSignup.options.w + ' .' + Venda.Widget.modalSignup.options.sP + 'Overlay').css('display', 'none');
+                        jQuery('.' + Venda.Widget.modalSignup.action.options.w + ' .' + Venda.Widget.modalSignup.action.options.sP).css('display', 'none');
+                        jQuery('.' + Venda.Widget.modalSignup.action.options.w + ' .' + Venda.Widget.modalSignup.action.options.sP + 'Overlay').css('display', 'none');
                     }, 3000);
                 }
             });
@@ -71,7 +73,7 @@ Venda.Widget.modalSignup = {
     checkForm: function (vfm) {
         var v = true,
             rEx = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-\+])+\.)+([a-zA-Z0-9]{2,4})+$/,
-            a = jQuery('.' + Venda.Widget.modalSignup.options.w + ' .' + Venda.Widget.modalSignup.options.sP + ' .invalidEmail').text();
+            a = jQuery('.' + Venda.Widget.modalSignup.action.options.w + ' .' + Venda.Widget.modalSignup.action.options.sP + ' .invalidEmail').text();
         e = ""
         if (!vfm.email.value || !vfm.email.value.match(rEx)) {
             v = false;
@@ -83,15 +85,15 @@ Venda.Widget.modalSignup = {
         return v;
     },
     displayPopup: function () {
-        if ((!closedPopup) && (!submittedPopup) && (unescape(location.href) !== Venda.Widget.modalSignup.options.uk) && (unescape(location.href) !== Venda.Widget.modalSignup.options.fr) && (unescape(location.href) !== Venda.Widget.modalSignup.options.de)) {
-            jQuery('.' + Venda.Widget.modalSignup.options.w + ' .' + Venda.Widget.modalSignup.options.sP).css('display', 'block');
-            jQuery('.' + Venda.Widget.modalSignup.options.w + ' .' + Venda.Widget.modalSignup.options.sP + 'Overlay').css('display', 'block');
+        if ((!closedPopup) && (!submittedPopup) && (unescape(location.href) !== Venda.Widget.modalSignup.action.options.uk) && (unescape(location.href) !== Venda.Widget.modalSignup.action.options.fr) && (unescape(location.href) !== Venda.Widget.modalSignup.action.options.de)) {
+            jQuery('.' + Venda.Widget.modalSignup.action.options.w + ' .' + Venda.Widget.modalSignup.action.options.sP).css('display', 'block');
+            jQuery('.' + Venda.Widget.modalSignup.action.options.w + ' .' + Venda.Widget.modalSignup.action.options.sP + 'Overlay').css('display', 'block');
         }
     },
     closePopup: function () {
         Venda.Widget.modalSignup.popupCookieClose.put('closedPopup', Venda.Widget.modalSignup.Close);
         closedPopup = Venda.Widget.modalSignup.popupCookieClose.get('closedPopup');
-        jQuery('.' + Venda.Widget.modalSignup.options.w + ' .' + Venda.Widget.modalSignup.options.sP).css('display', 'none');
-        jQuery('.' + Venda.Widget.modalSignup.options.w + ' .' + Venda.Widget.modalSignup.options.sP + 'Overlay').css('display', 'none');
+        jQuery('.' + Venda.Widget.modalSignup.action.options.w + ' .' + Venda.Widget.modalSignup.action.options.sP).css('display', 'none');
+        jQuery('.' + Venda.Widget.modalSignup.action.options.w + ' .' + Venda.Widget.modalSignup.action.options.sP + 'Overlay').css('display', 'none');
     }
 }
